d0f42bb5406e365c2d94d97cf0d53c7a1730be1e
Sparsh79 <60250698+Sparsh79@users.noreply.github.com>
2020-02-03 17:49:40 +0530

    Update HigherOrder.scala

--- src/main/scala/edu/knoldus/com/HigherOrder.scala
+++ src/main/scala/edu/knoldus/com/HigherOrder.scala
@@ -2,7 +2,7 @@ package edu.knoldus.com
 
 class HigherOrder {
   def sumofSquare(x: Int, y: Int): Int = x*x + y*y
-  def sumofCubes(x: Int, y: Int): Int = x*x + y*y
+  def sumofCubes(x: Int, y: Int): Int = x*x*y + y*y*y
   def sumofInt(x: Int, y: Int): Int = x+y
 
   def HigherOrder(f: (Int, Int) => Int, first: Int, second: Int): Int =  f(first, second)
@@ -13,10 +13,9 @@ object HigherOrder{
   def main(args: Array[String]): Unit = {
 
     val obj = new HigherOrder ()
-    println(obj.HigherOrder(obj.sumofSquare, first= 5, second = 5))
-      println(obj.HigherOrder(obj.sumofCubes, first = 7,second = 7))
-    println(obj.HigherOrder(obj.sumofInt, first = 8, second = 8))
-
+    println(obj.sumofSquare(x= 5, y= 5))
+      println(obj.sumofCubes(x = 7, y = 7))
+    println(obj.sumofInt(x = 8, y = 8))
 
   }
 }
